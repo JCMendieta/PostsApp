923f16040e5d66b0f2ff4d14bf88f4a4fef8c4c7
camilo <juank.yoh.117@gmail.com>
2025-04-19 16:02:10 -0500

    Initial project setup

---
+++ PostsApp/PostsAppTests/PostsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppTests.swift
+//  PostsAppTests
+//
+//  Created by Juan Camilo Mendieta Hernández on 19/04/25.
+//
+
+import Testing
+@testable import PostsApp
+
+struct PostsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
